984b5e156c5e85acc2a01f3a4b6e21b71fd8c56b
jackwardell <jackwardell@me.com>
2020-08-10 23:20:54 +0100

    minor upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="Wack",
-    version="0.1.1",
+    version="0.1.2",
     author="Jack Wardell",
     packages=find_packages(),
     include_package_data=True,
